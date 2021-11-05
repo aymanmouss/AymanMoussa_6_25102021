32a871e16a72d2ef2579c17762c0ed959831cda0
the-lime-dev <ayman@moussa.uk>
2021-11-05 16:17:14 +0100

    Filter Class

--- filter.js
+++ filter.js
@@ -1,1 +1,30 @@
+const navList = document.getElementsByClassName("nav-list");
+const tagList = document.querySelectorAll(".nav-list a li");
+const photographersId = document.getElementsByClassName("photographers-id");
 
+export default class filter {
+  filterTags(data) {
+    return navList[0].addEventListener("click", (e) => {
+      if (e.target.classList.value == "") {
+        Array.from(tagList).map((item) => {
+          if (item.classList.value === "active") {
+            item.classList.remove("active");
+          }
+        });
+        e.target.classList.add("active");
+      } else {
+        e.target.classList.remove("active");
+      }
+      data.photographers.map((item, i) => {
+        if (
+          !item.tags.includes(e.target.title) &&
+          e.target.classList.value === "active"
+        ) {
+          photographersId[i].classList.add("displaynone");
+        } else {
+          photographersId[i].classList.remove("displaynone");
+        }
+      });
+    });
+  }
+}
--- index.js
+++ index.js
@@ -1,37 +1,15 @@
 import photographerslist from "./photographerslist.js";
+import filter from "./filter.js";
 
 const photographerslist1 =
   document.getElementsByClassName("photographers-list");
-const navList = document.getElementsByClassName("nav-list");
-const tagList = document.querySelectorAll(".nav-list a li");
-const photographersId = document.getElementsByClassName("photographers-id");
 
 fetch("./photographers.json")
   .then((res) => res.json())
   .then((data) => {
-    navList[0].addEventListener("click", (e) => {
-      if (e.target.classList.value == "") {
-        Array.from(tagList).map((item) => {
-          if (item.classList.value === "active") {
-            item.classList.remove("active");
-          }
-        });
-        e.target.classList.add("active");
-      } else {
-        e.target.classList.remove("active");
-      }
-      data.photographers.map((item, i) => {
-        if (
-          !item.tags.includes(e.target.title) &&
-          e.target.classList.value === "active"
-        ) {
-          photographersId[i].classList.add("displaynone");
-        } else {
-          photographersId[i].classList.remove("displaynone");
-        }
-      });
-    });
-
+    // Filte Nav Tags Class
+    new filter().filterTags(data);
+    // photographers List Class
     data.photographers.map((data) => {
       photographerslist1[0].innerHTML += new photographerslist().photograph(
         data
